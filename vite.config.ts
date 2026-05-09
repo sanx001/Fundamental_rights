7db1e3ff415b772b3043cecb3bca52abd97ff81e
sanx001 <sanjaypraveena122006@gmail.com>
2026-05-09 22:38:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fundamental_rights/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
